3982dbf2eac08ad679c5f38dc152c131f4c54278
Bohdan Buhrak <bohdan0410@gmail.com>
2025-05-22 16:20:32 +0300

    corrects according to mentor's comments

--- src/main/java/mate/academy/spring/boot/intro/service/BookServiceImpl.java
+++ src/main/java/mate/academy/spring/boot/intro/service/BookServiceImpl.java
@@ -1,19 +1,15 @@
 package mate.academy.spring.boot.intro.service;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import mate.academy.spring.boot.intro.model.Book;
 import mate.academy.spring.boot.intro.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     private final BookRepository repository;
-
-    @Autowired
-    public BookServiceImpl(BookRepository repository) {
-        this.repository = repository;
-    }
 
     @Override
     public Book save(Book book) {
