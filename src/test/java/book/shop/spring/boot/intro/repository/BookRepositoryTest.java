9203b382dbe1ec5e324a1fc0f0276b6e4c836cf5
Bohdan Buhrak <bohdan0410@gmail.com>
2025-07-30 13:11:27 +0300

    Add JUnit test

--- src/test/java/book/shop/spring/boot/intro/repository/BookRepositoryTest.java
+++ src/test/java/book/shop/spring/boot/intro/repository/BookRepositoryTest.java
@@ -2,12 +2,15 @@ package book.shop.spring.boot.intro.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import book.shop.spring.boot.intro.config.TestRepositoryConfig;
 import book.shop.spring.boot.intro.model.Book;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -17,7 +20,7 @@ import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@DataJpaTest
 @Testcontainers
 class BookRepositoryTest {
 
