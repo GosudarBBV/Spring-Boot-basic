c27d11ba431989496ec34f9859420c20953ea505
Bohdan Buhrak <bohdan0410@gmail.com>
2025-07-30 13:53:35 +0300

    Add JUnit test

--- src/test/resources/database/clear-tables.sql
+++ src/test/resources/database/clear-tables.sql
@@ -1,3 +1,8 @@
 DELETE FROM books_categories;
+DELETE FROM cart_items;
+DELETE FROM orders;
+DELETE FROM order_items;
+DELETE FROM shopping_carts;
 DELETE FROM books;
 DELETE FROM categories;
+DELETE FROM users;
