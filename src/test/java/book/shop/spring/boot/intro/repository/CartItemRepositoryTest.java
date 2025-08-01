11340a925a8f675596b5fae9d80cb75872865f80
Bohdan Buhrak <bohdan0410@gmail.com>
2025-08-01 18:04:53 +0300

    Add JUnit test

--- src/test/java/book/shop/spring/boot/intro/repository/CartItemRepositoryTest.java
+++ src/test/java/book/shop/spring/boot/intro/repository/CartItemRepositoryTest.java
@@ -16,10 +16,10 @@ import org.springframework.test.context.jdbc.Sql;
 @DisplayName("CartItemRepository integration tests")
 @Sql(scripts = "classpath:database/schemas/cart-items-schema.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = {
-        "classpath:database/shopping-carts/add-shopping-cart-and-items.sql"
+        "classpath:database/cart-items/add-shopping-cart-and-items.sql"
 }, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = {
-        "classpath:database/shopping-carts/clear-shopping-cart-and-items.sql"
+        "classpath:database/cart-items/clear-shopping-cart-and-items.sql"
 }, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class CartItemRepositoryTest {
 
