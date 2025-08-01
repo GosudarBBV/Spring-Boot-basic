aee2f1699854a688c649a8cdf58960118cd7231c
Bohdan Buhrak <bohdan0410@gmail.com>
2025-08-01 13:22:37 +0300

    Add JUnit test

--- src/test/java/book/shop/spring/boot/intro/repository/BookReposytoryTestNew.java
+++ src/test/java/book/shop/spring/boot/intro/repository/BookReposytoryTestNew.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Set;
-
 import book.shop.spring.boot.intro.model.Book;
 import book.shop.spring.boot.intro.model.Category;
 import org.junit.jupiter.api.DisplayName;
