460bb7bc8dc8ce0a25ed6c186370fe1b5b9e2c52
Bohdan Buhrak <bohdan0410@gmail.com>
2025-08-02 10:58:07 +0300

    Add JUnit test

--- src/main/java/book/shop/spring/boot/intro/repository/BookRepository.java
+++ src/main/java/book/shop/spring/boot/intro/repository/BookRepository.java
@@ -1,13 +1,10 @@
 package book.shop.spring.boot.intro.repository;
 
 import book.shop.spring.boot.intro.model.Book;
-import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
     Page<Book> findAllByCategoriesId(Long id, Pageable pageable);
-
-    Optional<Book> findByIdAndDeletedFalse(Long id);
 }
