a91f38c5d97e9aad8e6e7589260cbb75ccf206d9
Bohdan Buhrak <bohdan0410@gmail.com>
2025-08-12 18:47:24 +0300

    add unit tests

--- src/main/java/book/shop/spring/boot/intro/repository/OrderRepository.java
+++ src/main/java/book/shop/spring/boot/intro/repository/OrderRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
-    @Query("SELECT oi FROM OrderItem oi JOIN FETCH oi.book WHERE oi.order.id = :orderId")
     Page<Order> findAllByUserId(Long userId, Pageable pageable);
 
     Optional<Order> findByIdAndUserId(Long orderId, Long userId);
