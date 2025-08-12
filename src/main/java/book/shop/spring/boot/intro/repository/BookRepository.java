2a2f5b182d029d1afed518f088a77b6949a3b031
Bohdan Buhrak <bohdan0410@gmail.com>
2025-08-12 11:26:28 +0300

    add unit tests

--- src/main/java/book/shop/spring/boot/intro/repository/BookRepository.java
+++ src/main/java/book/shop/spring/boot/intro/repository/BookRepository.java
@@ -3,9 +3,8 @@ package book.shop.spring.boot.intro.repository;
 import book.shop.spring.boot.intro.model.Book;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
     Page<Book> findAllByCategoriesId(Long id, Pageable pageable);
