6b31c0dc295ad376b59fb33ce5e25cf2027efee1
Bohdan Buhrak <bohdan0410@gmail.com>
2025-08-12 18:49:57 +0300

    add unit tests

--- src/main/java/book/shop/spring/boot/intro/repository/OrderRepository.java
+++ src/main/java/book/shop/spring/boot/intro/repository/OrderRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     Page<Order> findAllByUserId(Long userId, Pageable pageable);
