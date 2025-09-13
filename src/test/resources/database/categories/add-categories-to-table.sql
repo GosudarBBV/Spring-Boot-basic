2f92f709dc0970fb4f760ada6693ed8fbf7468b4
Bohdan Buhrak <bohdan0410@gmail.com>
2025-09-13 15:15:10 +0300

    update tests

--- src/test/resources/database/categories/add-categories-to-table.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO categories (id, name) VALUES (1, 'Test a');
-INSERT INTO categories (id, name) VALUES (2, 'Test b');
--- src/test/resources/database/shopping-carts/add-shopping-carts.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users (id, email, password, first_name, last_name, shipping_address)
-VALUES (1, 'test@cart.com', 'pass', 'Test', 'Cart', 'Kyiv');
-
-INSERT INTO users (id, email, password, first_name, last_name, shipping_address)
-VALUES (2, 'test2@cart.com', 'pass', 'Test2', 'Cart', 'Kyiv');
-
-INSERT INTO shopping_carts (id, user_id) VALUES (1, 1), (2, 2);
