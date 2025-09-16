fd8d127bdd05bf60f604dcc71f5cdc3bbad27657
Bohdan Buhrak <bohdan0410@gmail.com>
2025-09-16 21:18:52 +0300

    update tests

--- src/test/java/book/shop/spring/boot/intro/controller/BookControllerTest.java
+++ src/test/java/book/shop/spring/boot/intro/controller/BookControllerTest.java
@@ -22,13 +22,15 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Import(TestSecurityConfig.class)
 class BookControllerTest {
