f33409321f487eb19cf14f14f0821b5c716f0158
Gosudar <Bohdan0410@gmail.com>
2026-03-10 13:10:28 +0200

    update

--- src/test/resources/database/books/add-books-and-categories-into-table.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO books_categories (category_id, book_id)
-VALUES (1, 1), (1, 2);
--- src/test/resources/database/users/add-users.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (id, email, password, first_name, last_name, shipping_address)
-VALUES
-    (1, 'test@example.com', 'pass', 'Test', 'User', 'Kyiv'),
-    (2, 'john@example.com', 'pass', 'John', 'Doe', 'Lviv');
